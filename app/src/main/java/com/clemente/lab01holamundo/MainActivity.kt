5321c36c65c6b2c0b935d9d3cc582f87c14a8f4c
Nilton Clemente <nilton.clemente@tecsup.edu.pe>
2026-08-19 14:39:18 -0500

    “Proyecto inicial: Hola Mundo con Jetpack Compose”

---
+++ app/src/main/java/com/clemente/lab01holamundo/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.clemente.lab01holamundo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.clemente.lab01holamundo.ui.theme.Lab01HolaMundoTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.ui.unit.sp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Lab01HolaMundoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text(text = "¡Hola, soy $name!", fontSize = 24.sp)
+        Text(text = "Curso: Programación en Móviles")
+}
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Lab01HolaMundoTheme {
+        Greeting("Android")
+    }
+}
